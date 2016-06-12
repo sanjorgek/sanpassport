f131338f16829c32063a18761ca0463b07432a4f
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-06-12 03:47:42 -0500

    Ready to test

--- README.md
+++ README.md
@@ -4,9 +4,38 @@
   [![NPM Version][npm-image]][npm-url]
   [![NPM Downloads][downloads-image]][downloads-url]
 
-## Config
+## Settings
+First
 ~~~bash
 $ npm install sanpassport 
+~~~
+Then you need a valid user model/schema with constructor, findById, comparePassword and findOne functions. For example:
+~~~js
+var mongoose = require('mongoose'),
+  bcrypt = require('bcrypt');
+//...
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  name: {type: String, required: true},
+  apaterno: {type: String, required: true},
+  amaterno: {type: String, required: true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  admin: { type: Boolean, required: true }
+});
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+var userModel = mongoose.model('User', userSchema);
+~~~
+
+## Use
+~~~js
+var sanpassport = require('sanpassport')(userModel);
 ~~~
 
 
---
+++ index.js
@@ -0,0 +1,69 @@
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , debug = require('debug')('sanpassport')
+  , zxcvbn = require("zxcvbn");
+
+const MIN_PASSWORD_SCORE = 2;
+
+module.exports = function (userModel) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    userModel.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(new LocalStrategy(function(username, password, done) {
+    userModel.findOne({ username: username }, function(err, user) {
+      if (err) { debug(err);return done(err); }
+      if (!user) { debug("not user");return done(null, false, { message: 'Unknown user ' + username }); }
+      user.comparePassword(password, function(err, isMatch) {
+        if (err){ debug(err);return done(err);}
+        if(isMatch) {
+          return done(null, user);
+        } else {
+          debug("don't match");
+          return done(null, false, { message: 'Invalid password' });
+        }
+      });
+    });
+  }));
+
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+  };
+
+  function ensureAdmin(req, res, next) {
+    if(req.user && req.user.admin === true)
+        next();
+    else
+        res.send(403);
+  };
+
+  function createUser(userJson, done) {
+    var result = zxcvbn(userJson.password);
+    if (result.score < MIN_PASSWORD_SCORE) return done(new Error("Password is too simple"));
+    var user = new userModel(userJson);
+
+    user.save(function(err) {
+        if(err) {
+            done(err);
+        } else {
+            done(null, user);
+        }
+    });
+
+  };
+
+  return {
+    ensureAuthenticated: ensureAuthenticated,
+
+    ensureAdmin: ensureAdmin,
+
+    createUser : createUser
+  }
+}
